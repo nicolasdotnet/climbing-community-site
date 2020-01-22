a12af946bdc55cb66947f52e2464f9ce5a0d86ee
nicolasdotnet <nicolas.desdevises@gmail.com>
2020-01-22 10:52:39 +0100

    commit the spring boot framework and update the gitignore file

---
+++ src/main/java/org/amisescalade/ClimbingCommunitySiteApplication.java
@@ -0,0 +1,13 @@
+package org.amisescalade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingCommunitySiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingCommunitySiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/amisescalade/ClimbingCommunitySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amisescalade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingCommunitySiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
